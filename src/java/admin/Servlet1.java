60a344053241ad94229a5608b910c08e9fc470b4
12metalx <perezozo79@gmail.com>
2020-03-08 13:30:10 -0600

    Agregacion xml con usuarios, validacion de usuario y contrasena
    Se agrego el xml que hace funcion de base de datos, con algunos
    usuarios de ejemplo, ademas se agrego un metodo de busqueda de
    usuario y validacion de contrasena, con respuesta de un tipo de
    dato entero

--- src/java/admin/Servlet1.java
+++ src/java/admin/Servlet1.java
@@ -20,23 +20,37 @@ public class Servlet1 extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out=response.getWriter();
+        String ruta = request.getRealPath("/");
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
         try
         {
         SAXBuilder builder = new SAXBuilder();
-        File archivoXML = new File("archivoXML.xml");
+        File archivoXML = new File(ruta + "\\archivoXML.xml");
         Document documento=builder.build(archivoXML);
         Element raiz=documento.getRootElement();
-        List lista=raiz.getChildren("name");
-            for(int i=0;i<lista.size();i++)
-            {
-             Element elemento=(Element)lista.get(i);
-             String cadena=elemento.getTextTrim();
-             out.println(cadena);
-            }
+        List lista=raiz.getChildren("usuario");
+            out.println(validateUP(lista, user, pass));
         }
         catch(JDOMException e)
         {
         e.printStackTrace();
         }
         }
+    
+    static int validateUP(List lista,String user,String pass){
+        for(int i=0;i<lista.size();i++)
+            {
+                Element elemento=(Element)lista.get(i);
+                String cadena=elemento.getAttributeValue("username");
+                if(cadena.equals((Object)user)){
+                    cadena = elemento.getAttributeValue("password");
+                    if(cadena.equals((Object)pass)){
+                        return 1;
+                    }
+                }
+            }
+            return 0;
+    
+    }
 }
